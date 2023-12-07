07de366d4317752428a38e54372b1b47a5b68250
lisapowell1991 <lisapowell46@gmail.com>
2023-12-07 05:27:16 -0800

    distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,9 +5,9 @@ a Module that contains a do_deploy function.
 
 from fabric.api import env, run, put
 from os.path import exists
+from datetime import datetime
 
 env.hosts = ['54.237.218.141', '34.232.53.132']
-env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
@@ -20,28 +20,31 @@ def do_deploy(archive_path):
         return False
 
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/")
+        # Get the archive file name without extension
+        archive_name = archive_path.split('/')[-1].split('.')[0]
 
-        # Extract the archive to /data/web_static/releases/
-        # <archive filename without extension>
-        filename = archive_path.split('/')[-1]
-        folder_name = f"/data/web_static/releases/{filename.split('.')[0]}"
-        run(f"mkdir -p {folder_name}")
-        run(f"tar -xzf /tmp/{filename} -C {folder_name}")
+        # Upload the archive to the /tmp/ directory on the web server
+        put(archive_path, '/tmp/')
+
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive_name>
+        run(f"mkdir -p /data/web_static/releases/{archive_name}")
+        run(f"tar -xzf /tmp/{archive_path.split('/')[-1]} "
+            f"-C /data/web_static/releases/{archive_name}")
 
         # Delete the archive from the web server
-        run(f"rm /tmp/{filename}")
+        run(f"rm /tmp/{archive_path.split('/')[-1]}")
 
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("rm -f /data/web_static/current")
+        # Delete the symbolic link /data/web_static/current
+        run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link /data/web_static/current
-        # linked to the new version
-        run(f"ln -s {folder_name} /data/web_static/current")
+        run(f"ln -s /data/web_static/releases/{archive_name} "
+            f"/data/web_static/current")
 
+        print("New version deployed!")
         return True
 
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
         return False
