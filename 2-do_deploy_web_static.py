eb365eac0c8269834d08d04d15e9680fb01317c0
lisapowell1991 <lisapowell46@gmail.com>
2023-12-06 18:44:46 -0800

    a Fabric script that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,41 +5,43 @@ a Module that contains a do_deploy function.
 
 from fabric.api import env, run, put
 from os.path import exists
-from datetime import datetime
 
 env.hosts = ['54.237.218.141', '34.232.53.132']
 env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
+    """
+    A Fabric script that distributes an archive to your web servers
+    """
+
     # Check if the archive file exists
     if not exists(archive_path):
         return False
 
     try:
-        # Upload the archive to the /tmp/ directory on the web servers
+        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
 
-        # Get the archive filename without extension
-        archive_filename = archive_path.split("/")[-1]
-        archive_name_no_ext = archive_filename.split(".")[0]
+        # Extract the archive to /data/web_static/releases/
+        # <archive filename without extension>
+        filename = archive_path.split('/')[-1]
+        folder_name = f"/data/web_static/releases/{filename.split('.')[0]}"
+        run(f"mkdir -p {folder_name}")
+        run(f"tar -xzf /tmp/{filename} -C {folder_name}")
 
-        # Uncompress the archive to the releases folder on the web servers
-        run(f"mkdir -p /data/web_static/releases/{archive_name_no_ext}")
-        run(f"tar -xzf /tmp/{archive_filename} "
-            f"-C /data/web_static/releases/{archive_name_no_ext}")
+        # Delete the archive from the web server
+        run(f"rm /tmp/{filename}")
 
-        # Delete the archive from the web servers
-        run(f"rm /tmp/{archive_filename}")
-
-        # Delete the existing symbolic link
+        # Delete the symbolic link /data/web_static/current from the web server
         run("rm -f /data/web_static/current")
 
-        # Create a new symbolic link to the new version of the code
-        run(f"ln -s /data/web_static/releases/{archive_name_no_ext} "
-            f"/data/web_static/current")
+        # Create a new symbolic link /data/web_static/current
+        # linked to the new version
+        run(f"ln -s {folder_name} /data/web_static/current")
 
         return True
 
     except Exception as e:
+        print(e)
         return False
