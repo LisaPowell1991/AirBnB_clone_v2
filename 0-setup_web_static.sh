e438f6d207475da5b2ff46f8bf9a793af221395d
Agbode-Milan <agbodekacalebkm@gmail.com>
2023-12-07 15:28:12 -0800

    a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,39 +1,44 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Install Nginx if not already installed
-sudo apt-get update
-sudo apt-get install -y nginx
+# Set directory structures
+data_dir="/data"
+web_static_dir="$data_dir/web_static"
+releases_dir="$web_static_dir/releases"
+shared_dir="$web_static_dir/shared"
+test_dir="$releases_dir/test"
 
-# Create necessary folders if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
-
-# Create a fake HTML file for testing
-echo "<html>
- <head>
- </head>
- <body>
-  Holberton School
- </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# Create or recreate a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership to ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx config to serve /data/web_static/current at /hbnb_static/
-nginx_config="/etc/nginx/sites-available/default"
-nginx_config_backup="$nginx_config.bak" # Backup the original config
-
-# Backup the original config if not backed up already
-if [ ! -e "$nginx_config_backup" ]; then
-    sudo cp "$nginx_config" "$nginx_config_backup"
+# Install Nginx
+if ! command -v nginx &> /dev/null; then
+  echo "Installing Nginx..."
+  sudo apt-get update
+  sudo apt-get install -y nginx
 fi
 
-# Add or update the location block in Nginx config
-sudo sed -i "/location \/ {/a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" "$nginx_config"
+# Create necessary directories
+mkdir -p "$data_dir" "$web_static_dir" "$releases_dir" "$shared_dir"
+mkdir -p "$test_dir"
 
-# Restart Nginx to apply changes
+# Create test HTML file
+echo "<html>
+<head>
+</head>
+<body>
+  Holberton School
+</body>
+</html>" > "$test_dir/index.html"
+
+# Create/recreate symbolic link
+rm -f "$web_static_dir/current" || true
+ln -sf "$test_dir" "$web_static_dir/current"
+
+# Set ownership
+sudo chown -R ubuntu:ubuntu "$data_dir"
+
+# Update Nginx configuration
+sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+# Restart Nginx
 sudo service nginx restart
+
+echo "Web server setup complete!"
 
