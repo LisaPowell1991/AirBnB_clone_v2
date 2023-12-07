6302a79a8d19576d096484c55b0622ee3eb0ea63
Agbode-Milan <agbodekacalebkm@gmail.com>
2023-12-07 15:14:44 -0800

    a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,39 @@
 #!/usr/bin/env bash
-# sets up my web servers for the deployment of web_static
 
 # Install Nginx if not already installed
 sudo apt-get update
 sudo apt-get install -y nginx
 
-# Create folders if not already exists
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+# Create necessary folders if they don't exist
+sudo mkdir -p /data/web_static/{releases/test,shared}
 
 # Create a fake HTML file for testing
 echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >> /data/web_static/releases/test/index.html
+ <head>
+ </head>
+ <body>
+  Holberton School
+ </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create & recreate a symbolic link
+# Create or recreate a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership to ubuntu user and group
+# Give ownership to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
-# update Nginx config to serve /data/web_static/current at /hbnb_static/
-sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+# Update Nginx config to serve /data/web_static/current at /hbnb_static/
+nginx_config="/etc/nginx/sites-available/default"
+nginx_config_backup="$nginx_config.bak" # Backup the original config
+
+# Backup the original config if not backed up already
+if [ ! -e "$nginx_config_backup" ]; then
+    sudo cp "$nginx_config" "$nginx_config_backup"
+fi
+
+# Add or update the location block in Nginx config
+sudo sed -i "/location \/ {/a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" "$nginx_config"
 
 # Restart Nginx to apply changes
 sudo service nginx restart
+
