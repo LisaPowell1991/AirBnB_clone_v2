2823b192caaf9b0d0d87d6f1a9116bb0abbd863a
Agbode-Milan <agbodekacalebkm@gmail.com>
2023-12-07 15:10:17 -0800

    a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
-#set things up for deployment
+# sets up my web servers for the deployment of web_static
 
-if [ ! -x /usr/sbin/nginx ]; then
-	sudo apt-get update -y -qq && \
-	    sudo apt-get install -y nginx
-fi
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get install -y nginx
 
+# Create folders if not already exists
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-# make dirs...
-sudo mkdir -p /data/web_static/releases/test  /data/web_static/shared/
+# Create a fake HTML file for testing
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
 
+# Create & recreate a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-echo "<h1> I couldn't center a div </h1>" | sudo dd status=none of=/data/web_static/releases/test/index.html
-
-# create sym link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
+# Give ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
+# update Nginx config to serve /data/web_static/current at /hbnb_static/
+sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
-sudo cp /etc/nginx/sites-enabled/default /etc/nginx/nginx-sites-enabled_default.bck
-
-
-sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
+# Restart Nginx to apply changes
 sudo service nginx restart
