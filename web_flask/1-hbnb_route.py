48da304dccb9be1dbebbc2f220d786cc6102ede6
lisapowell1991 <lisapowell46@gmail.com>
2023-12-23 05:11:27 -0800

    script that starts a Flask web application with 2 routes / and /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+a script that starts a Flask web application,
+that must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route function for the root URL("/")
+
+    Returns:
+    str: A greeting message "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route function for the "/hbnb URL.
+
+    Returns:
+    str: A message "HBNB".
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    """ Run the Flask web app """
+    app.run(host='0.0.0.0', port=5000)
