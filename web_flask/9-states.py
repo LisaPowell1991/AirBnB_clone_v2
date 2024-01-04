5fac271bd08898b12169d0cd861274ef0efecd65
lisapowell1991 <lisapowell46@gmail.com>
2024-01-04 05:06:11 -0800

    a script that starts a Flask web application

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """
+    Teardown method to close the SQLAlchemy Session after each request
+    """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def display_states():
+    """
+    Display a list of all states
+    """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def display_cities_by_state(id):
+    """
+    display cities of a specific state.
+    """
+    state = storage.get("State", id)
+    if state:
+        cities = sorted(state.cities, key=lambda x: x.name)
+        if hasattr(state, 'cities') else state.cities
+        return render_template('9-state.html', state=state, cities=cities)
+    else:
+        return render_template('9-state.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
